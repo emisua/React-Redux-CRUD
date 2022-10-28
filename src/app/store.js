626de7d1c330425f2ba146b198fbbd274d851e72
emisua <91371538+emisua@users.noreply.github.com>
2022-10-28 09:44:59 +0200

    2 files modified, 2 deleted

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,5 @@
 import { configureStore } from '@reduxjs/toolkit';
-import notasReducer from '../features/NotaSlice';
 
 export default configureStore({
-  reducer: {
-    notas: notasReducer,
-  },
+  reducer: {},
 });
